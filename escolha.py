8162c00080b0412eb6861b16036904b7ff31a513
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-03-03 16:54:17 -0300

    Update and rename screenLR.py to escolha.py

---
+++ escolha.py
@@ -0,0 +1,16 @@
+import random
+import os
+
+
+maquina_escolheu = random.randrange(0, 9)
+
+vc_escolheu = int(input("escolha um numero de 0 a 9:"))
+
+if maquina_escolheu == vc_escolheu:
+    os.system("clear")
+    print("parabéns vc acertou")
+
+else:
+    os.system("clear")
+    print("a maquina escolheu \n", maquina_escolheu)
+    print("vc escolheu escolheu \n", vc_escolheu)
